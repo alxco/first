ad14483b0f08bf3fb51059069f043c7140d598af
Alexander <alexander.cozuberda@gmail.com>
2015-05-29 15:47:14 -0700

    1

--- libpyutils/setup.py
+++ libpyutils/setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-
+pkgs = find_packages(exclude=['docs', 'testspec'])
+print ('pkgs', pkgs)
 setup(
     name='MyFirstPkg',
     version='0.1.1',
-    packages=find_packages(exclude=['docs', 'testspec']),
+    packages=pkgs,
     install_requires=['mockito==0.5.2']
 )
